6d6a463051c0ff3d4d8de3fb754d8a2fae2ccc0c
Kai <akaikainazar@gmail.com>
2024-06-02 14:23:45 +0500

    changes

--- src/components/LearningPageSlider.tsx
+++ src/components/LearningPageSlider.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation } from 'swiper/modules'
 import 'swiper/css'
@@ -8,6 +9,8 @@ import SliderElement from './SliderElement'
 import sliderArrow from '../assets/images/slider-arrow.svg'
 
 export default function LearningPageSlider() {
+    const navigate = useNavigate()
+
     const w = window.innerWidth
     return (
         <div className="swiper-slider-container">
@@ -22,67 +25,67 @@ export default function LearningPageSlider() {
                 loop={true}
                 className="mySwiper"
             >
-                <SwiperSlide>
+                <SwiperSlide onClick={() => navigate('/topic1')}>
                     <SliderElement
                         heading="С++ деген не?"
                         text="Бұл параграфта біз С++ -пен танысамыз"
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide onClick={() => navigate('/topic2')}>
                     <SliderElement
                         heading="Кіріспе"
                         text="Бұл параграфта біз С++ -тің негізін оқимыз"
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide onClick={() => navigate('/topic3')}>
                     <SliderElement
                         heading="Бірінші қадамдар"
                         text="Бұл параграфта біз бірінші C++ тілінде бағдарлама жасап, оны басып шығаруды және пернетақтадан жолдар мен сандарды оқуды үйренеміз."
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide onClick={() => navigate('/topic4')}>
                     <SliderElement
                         heading="Мәліметтер типі"
                         text="Бұл сабақта біз С++ тіліндегі түрлі мәліметтер типтерін үйренетін боламыз"
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide onClick={() => navigate('/topic5')}>
                     <SliderElement
                         heading="Шарттар"
                         text="Бұл параграфта біз if және switch тармақтау операторларымен танысамыз."
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide onClick={() => navigate('/topic6')}>
                     <SliderElement
                         heading="Тармақтану және циклдер"
                         text="Бұл параграфта біз циклдерді үйренетін боламыз"
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide onClick={() => navigate('/topic7')}>
                     <SliderElement
                         heading="Құрама мәліметтер типі"
                         text="Бұл параграфта құрама мәліметтер типін оқитын боламыз"
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide onClick={() => navigate('/topic8')}>
                     <SliderElement
                         heading="Сілтеме, нұсқағыш, тұрақты шама"
                         text="С++ тіліндегі қосалқы құралдарды үйренеміз"
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide onClick={() => navigate('/topic9')}>
                     <SliderElement
                         heading="Функциялар"
                         text="Бұл сабақта біз С++ тіліндегі функциялармен танысатын боламыз"
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide onClick={() => navigate('/topic10')}>
                     <SliderElement
                         heading="Үлгілер"
                         text="Бұл параграфта біз есептер шығарамыз"
                     />
                 </SwiperSlide>
-                <SwiperSlide>
+                <SwiperSlide onClick={() => navigate('/topic11')}>
                     <SliderElement
                         heading="Тарау бойынша тапсырмаларды талдау"
                         text="Бұл сабақта біз тарау бойынша бекітетін тапсырмаларды орындаймыз"
