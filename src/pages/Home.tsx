8fed6f9f48bac60d29745ebfb23d3fea0048b6e2
Kai <akaikainazar@gmail.com>
2024-06-16 22:29:53 +0500

    RTK

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { useAppSelector, useAppDispatch } from '../app/hooks'
+import { useAppSelector } from '../app/hooks'
 
 import linkIcon from '../assets/images/link-icon.svg'
 
